20241002aa2cf968fdd5693eb89ea57ad6ff815a
igr <mixa_fc@ukr.net>
2017-11-07 15:14:43 +0200

    1 плеер готов

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,11 @@ import Music.Player1;
 
 public class Main {
     public static void main(String[] args) {
-        Mp3players mp3players = new Mp3players("The best Song", "The best Song" "Good song" "Super Song");
-        Player1 player1 = new Player1("The best Song", "The best Song",100.0);
+        String [] playList = {"The best Song", "Good song", "Super Song"};
+        Mp3players mp3players = new Mp3players("The best Song", playList);
+        Player1 player1 = new Player1("The best Song", playList,100.0);
 
-        System.out.println();
+         player1.playSong();
     }
 
 }
--- src/Music/Player1.java
+++ src/Music/Player1.java
@@ -12,4 +12,14 @@ public class Player1 extends Mp3players {
     public double getPrice() {
         return price;
     }
+
+    public void playSong(){
+        String getSong;
+
+        if(getSong() != null)
+            getSong = getSong();
+        else
+            getSong = " select a song ";
+        System.out.println("Playing: " + getSong);
+    }
 }
